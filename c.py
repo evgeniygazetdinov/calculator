054eba5d19e5162f057afec14b196cc092a5aaab
evgeniy gazetdinov <zplacesound@gmail.com>
2019-08-02 21:35:45 +0300

    Add files via upload

--- c.py
+++ c.py
@@ -2,18 +2,29 @@
 
 import tkinter as tk
 
-def fin():
-    print(entry1.get())
+operation = tk.StringVar()
+
+def equlize():
+    f_n = first_number.get()
+    s_n = second_number.get()
+    operator = operation.get()`
+
 
 root= tk.Tk()
 
 canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
-entry1 = tk.Entry (root)
+first_number = tk.Entry (root)
 canvas1.create_window(210, 100, window=entry1)
-but = tk.Button(root,command =fin)
+second_number = tk.Entry (root)
+canvas1.create_window(210, 100, window=entry1)
+#operations
+divider = tk.Button(root,name = '/',command = lambda :operation.set('/'))
 canvas1.create_window(220, 110, window=but)
-
-
-
+multiply = tk.Button(root,name = '*',command = lambda :operation.set('*'))
+canvas1.create_window(220, 110, window=but)
+plus = tk.Button(root,name = '+',command =lambda :operation.set('+'))
+canvas1.create_window(220, 110, window=but)
+eq = tk.Button(root,name = '=',command = equlize)
+canvas1.create_window(220, 110, window=but)
 root.mainloop()
